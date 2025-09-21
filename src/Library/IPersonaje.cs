752949eec096d89793d4798313504aa5c940ba98
Renzo <renzoberetta27@gmail.com>
2025-09-21 19:10:05 -0300

    lo que seria el IPersonaje maso

---
+++ src/Library/IPersonaje.cs
@@ -0,0 +1,8 @@
+﻿public interface IPersonaje
+{
+    string Nombre { get; }
+    int AtaqueTotal();
+    int DefensaTotal();
+    void Atacar(IPersonaje objetivo);
+    void Curar(IPersonaje aliado);
+}
