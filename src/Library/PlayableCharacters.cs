eeccff0e05502e7d01873b50ac65cbcd7f4d9d22
joaco2801 <diazernestojoaquin@gmail.com>
2025-10-21 17:44:29 -0300

    refactorización

--- src/Library/PlayableCharacters.cs
+++ src/Library/PlayableCharacters.cs
@@ -4,11 +4,7 @@ namespace Program
     public class Magos : CreadorDePersonajes // parametros del mago
     {
         public Magos(string nombre)
-            : base(nombre, 80, 10, 5)
-        {
-            
-        }
-
+            : base(nombre, 80, 10, 5) { }
         public override void BreveDescripcion()
         {
             Console.WriteLine($"{Nombre} lanza poderosos hechizos desde un misterioso libro con distintas runas...  Está un poco flaco.");
@@ -19,7 +15,6 @@ namespace Program
     {
         public Elfos(string nombre) 
             : base(nombre, 100, 8, 12) { }
-
         public override void BreveDescripcion()
         {
             Console.WriteLine($"{Nombre}, normalmente con arcos, normalmente con cuchillos, ayuda al equipo de diferentes formas... No es muy amistoso.");
@@ -30,7 +25,6 @@ namespace Program
     {
         public Enanos(string nombre) 
             : base(nombre, 150, 15, 13) { }
-
         public override void BreveDescripcion()
         {
             Console.WriteLine($"{Nombre} tiene un hacha y ¿¡Una olla!? Gran cocinero y mejor persona... Es la voz de la experiencia.");
